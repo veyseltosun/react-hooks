a5c3e150f663573b5ff0616f4b8f85f1c20fb814
veyseltosun <postalarim1923@gmail.com>
2023-01-20 00:20:19 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="App">
-      <TodoList/>
+      <TodoList />
     
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 
 const TodoList = () => {
+    cosnt [todos, setTodo] = useState()
     return(
         <div>
             <ul>
