8abc927b20bcd14abbc8a7ed1cd897c8ff043f98
veyseltosun <postalarim1923@gmail.com>
2023-01-19 23:09:36 +0300

    ....

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TodoList = () => {
+    return(
+        <div>
+            <ul>
+                <li>Pay bills</li>
+                <li>Do your home work</li>
+                <li>Feed the dog</li>
+            </ul>
+        </div>
+    )
+}
+export default TodoList;
